7d872f9f83c38fccdbcbb6a191579c6c13a0bb46
torre <torre@LAPTOP-4GQN8FL3>
2023-04-18 17:09:22 -0600

    Agregar archivos de proyecto.

---
+++ clase abstracta/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_abstracta
+{
+    public abstract class TrianguloB
+    {
+        public abstract void Perimetro();
+
+        public abstract void Area();
+
+    }
+    public class Escaleno : TrianguloB
+    {
+        private double num1 = 9;
+        private double num2 = 4;
+        public override void Area()
+        {
+            Console.WriteLine("ESCALENO");
+            Console.WriteLine();
+            Console.WriteLine("El area es: " + (num1*num2)/2);
+        }
+
+        public override void Perimetro()
+        {
+            Console.WriteLine("El perimetro es: " + (num1 + num2));
+        }
+    }
+    public class Acutangulo : TrianguloB
+    {
+        private double num1= 5;
+        private double num2= 10;
+        public override void Area()
+        {
+            Console.WriteLine("TRIÀNGULO");
+            Console.WriteLine();
+            Console.WriteLine("El area es: "+ (num1*num2/2));
+        }
+
+        public override void Perimetro()
+        {
+            Console.WriteLine("El perimetro es: " + (num1+num2));
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Acutangulo acutangulo= new Acutangulo();
+            Escaleno escaleno= new Escaleno(); 
+            
+            acutangulo.Area();
+            acutangulo.Perimetro();
+            Console.WriteLine();
+            escaleno.Area();
+            escaleno.Perimetro();
+            Console.WriteLine();
+        }
+    }
+}
